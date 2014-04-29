4ec1b7d02e735d71e68809182fb74f39d5ea3d91
puhrome <phirom.yim@fullsail.edu>
2014-04-29 17:54:19 -0400

    Returning Values in a Function
    Understanding that a Return Value is what comes out of a Function. Used
    WebStorm editor.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/**
+ * Returning Values Created by Phirom Yim on 4/29/14.
+ */
+
+var total = calcArea(30, 20);//assign it to function call
+
+function calcArea(w, h) {//w=30, h=20
+    var area = w * h;//variable for are
+    return area;//what we are returning, function spitting info out
+    //returning in the argument
+}
+console.log(total);
